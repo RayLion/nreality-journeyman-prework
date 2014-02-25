7bf6736578a2574a0af57e536f6539525be57e03
RayLion <raynoels@gmail.com>
2014-02-25 16:09:44 +0200

    Fizzbuzz

---
+++ FizzBuzzTdd/Calculate.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace FizzBuzzTdd
+{
+    public class Calculate
+    {
+        private static string _result;
+
+        public static string CalculateOutcome(int number)
+        {
+            _result = string.Empty;
+
+            AddFizzToResultIfDivisableByThree(number);
+            AddBuzzToResultIfDivisableByFive(number);
+            SetResultToNumberIfNotDivisableByThreeOrFive(number);
+
+            return _result;
+        }
+
+        private static void SetResultToNumberIfNotDivisableByThreeOrFive(int number)
+        {
+            if (string.IsNullOrEmpty(_result))
+                _result = number.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private static void AddFizzToResultIfDivisableByThree(int number)
+        {
+            if (number%3 == 0)
+                _result += "Fizz";
+        }
+
+        private static void AddBuzzToResultIfDivisableByFive(int number)
+        {
+            if (number%5 == 0)
+                _result += "Buzz";
+        }
+    }
+}
---
+++ FizzBuzzTdd/FizzBuzzFixture.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+
+namespace FizzBuzzTdd
+{
+    [TestFixture]
+    public class FizzBuzzFixture
+    {
+        [TestCase(3)]
+        [TestCase(6)]
+        public void GivenNumberThatIsDivisableByThree_WhenCalculateOutcome_FizzIsReturned(int number)
+        {
+            var answer = Calculate.CalculateOutcome(number);
+            Assert.AreEqual("Fizz", answer);
+        }
+
+        [TestCase(5)]
+        [TestCase(10)]
+        public void GivenNumberThatIsDivisableByFive_WhenCalculateOutcome_BuzzIsReturned(int number)
+        {
+            var answer = Calculate.CalculateOutcome(number);
+            Assert.AreEqual("Buzz", answer);
+        }
+
+        [TestCase(15)]
+        public void GivenNumberThatIsDivisableByThreeAndFive_WhenCalculate_ThenFizzBuzz(int number)
+        {
+            var answer = Calculate.CalculateOutcome(number);
+            Assert.AreEqual("FizzBuzz", answer);	
+        }
+        
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        public void GivenNumberThatAreNotDivisableByThreeOrFive_WhenCalculate_ThenNumberIsReturned(int number)
+        {
+            var answer = Calculate.CalculateOutcome(number);
+            Assert.AreEqual(number.ToString(CultureInfo.InvariantCulture), answer);	
+        }
+    }
+}
